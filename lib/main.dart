d4e5085fc7f32d0bdb9c1dac051e02321fdd5dac
yogesh4216 <yogesh2005m@gmail.com>
2026-05-17 23:20:29 +0530

    Add HttpOverrides SSL bypass for local ngrok certificate compatibility on Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -6,7 +7,16 @@ import 'screens/step_guide_screen.dart';
 import 'screens/live_cpr_screen.dart';
 import 'screens/chat_screen.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   SystemChrome.setSystemUIOverlayStyle(
